b4199febfcfd8f99abe917f2b1719e7387d25171
Brandon Durand <ptco.dev@gmail.com>
2024-09-10 13:57:09 -0500

    Update Oauth.jsx

--- client/src/components/universal/Oauth.jsx
+++ client/src/components/universal/Oauth.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Oauth = ({type, icon, color}) => {
     return (
-        <Link to={`http://localhost:5000/${type}`} className="bg-white border border-2 p-1 rounded mx-1 fa-beat" style={{animationIterationCount: 1, animationDuration: ".3s", animationDelay: '.15s'}}>
+        <Link to={`${process.env.REACT_APP_BACKEND_URL}/${type}`} className="bg-white border border-2 p-1 rounded mx-1 fa-beat" style={{animationIterationCount: 1, animationDuration: ".3s", animationDelay: '.15s'}}>
             <i className={`fa-brands fa-${icon} fa-2x`} style={{color: color ? color:null}}></i>
         </Link>
     );
