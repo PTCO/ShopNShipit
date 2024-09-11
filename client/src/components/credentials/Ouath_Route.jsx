d662e98c15935133d6be0a7a8b70b4b641431331
Brandon Durand <ptco.dev@gmail.com>
2024-09-11 01:32:04 -0500

    Update Ouath_Route.jsx

--- client/src/components/credentials/Ouath_Route.jsx
+++ client/src/components/credentials/Ouath_Route.jsx
@@ -1,18 +1,20 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Cookie from 'js-cookie';
 import UserContext from "../../context/userContext";
 import axios from "axios";
 
 const Oauth_Route = () => {
-    const { actions, authCookie, OauthCookie } = useContext(UserContext);
+    const { actions,  } = useContext(UserContext);
     const location = useLocation();
+    const [OauthCookie, setOauthCookie] = useState(null);
 
     useEffect(()=>{
         const sid = location.pathname.split("/")[3]
         Cookie.set('usc', JSON.stringify(sid));
+        setOauthCookie(JSON.stringify(Cookie.get('usc')).substring(3, 35))
         (async()=>{
-            await axios.get(`${process.env.REACT_APP_BACKEND_URL}/User/${OauthCookie ? OauthCookie:authCookie}`)
+            await axios.get(`${process.env.REACT_APP_BACKEND_URL}/User/${OauthCookie}`)
             .then( result => { actions.setUser(result.data); actions.navigate(location.pathname === '/Login' || location.pathname === '/Signin' ? '/Home':location.pathname) }) // Change navigate route as needed
             .catch( error => actions.handleError(error.response))
         })()
