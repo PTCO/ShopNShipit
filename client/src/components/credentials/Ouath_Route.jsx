ba3b8f2204b55de3a661caacf2113c421709da11
Brandon Durand <ptco.dev@gmail.com>
2024-09-10 23:01:02 -0500

    Update Ouath_Route.jsx

--- client/src/components/credentials/Ouath_Route.jsx
+++ client/src/components/credentials/Ouath_Route.jsx
@@ -4,21 +4,15 @@ import Cookie from 'js-cookie';
 import UserContext from "../../context/userContext";
 
 const Oauth_Route = () => {
-
     const { actions } = useContext(UserContext);
-
     const location = useLocation();
 
     useEffect(()=>{
-        const sid = location.pathname.split("/")[1]
+        const sid = location.pathname.split("/")[2]
         console.log(sid, location.pathname);
         Cookie.set('usc', sid);
         actions.navigate('/Home');
     }, [])
-
-    return (
-        <h1>Hello</h1>
-    )
 }
 
 export default Oauth_Route;
