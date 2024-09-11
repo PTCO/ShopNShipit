18f7aff77305b0773ab69ccd9b80dbf9503184aa
Brandon Durand <ptco.dev@gmail.com>
2024-09-11 01:34:03 -0500

    Update Ouath_Route.jsx

--- client/src/components/credentials/Ouath_Route.jsx
+++ client/src/components/credentials/Ouath_Route.jsx
@@ -12,7 +12,7 @@ const Oauth_Route = () => {
     useEffect(()=>{
         const sid = location.pathname.split("/")[3]
         Cookie.set('usc', JSON.stringify(sid));
-        setOauthCookie(JSON.stringify(Cookie.get('usc')).substring(3, 35))
+        setOauthCookie(sid)
         (async()=>{
             await axios.get(`${process.env.REACT_APP_BACKEND_URL}/User/${OauthCookie}`)
             .then( result => { actions.setUser(result.data); actions.navigate(location.pathname === '/Login' || location.pathname === '/Signin' ? '/Home':location.pathname) }) // Change navigate route as needed
