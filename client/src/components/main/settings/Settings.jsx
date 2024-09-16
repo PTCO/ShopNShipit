5b04f45aa6c69d711cf5b0f2572a8a65aa69a229
Brandon Durand <ptco.dev@gmail.com>
2024-09-16 17:18:53 -0500

    Update Responsive CSS

--- client/src/components/main/settings/Settings.jsx
+++ client/src/components/main/settings/Settings.jsx
@@ -7,13 +7,13 @@ const Settings = () => {
 
     return (
         <div>
-            <div className="my-2">
+            <div className="my-sm-2 col-md-6">
                 <h2 className="border-bottom border-2 pb-1 mb-2 settingsTitle" >Orders</h2>
                 <ul>
                     <li className="p-2 border border-2 rounded fw-bold" onClick={ e => actions.navigate('/Home/Settings/Orders')}><i class="fa-solid fa-box fa-xl"></i> Your Orders</li>
                 </ul>
             </div>
-            <div className="mb-2">
+            <div className="mb-2 col-md-6">
                 <h2 className="border-bottom border-2 pb-1 mb-2 settingsTitle" >Account</h2>
                 <ul>        
                     <li className="p-2 border border-2 rounded fw-bold" onClick={ e => actions.navigate('/Home/Settings/Profile')}><i class="fa-solid fa-user fa-xl"></i> Profile</li>
