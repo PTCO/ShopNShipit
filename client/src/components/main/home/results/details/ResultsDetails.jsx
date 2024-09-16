835923b18bbebfe24733978f9a7de8a013cc4a06
Brandon Durand <ptco.dev@gmail.com>
2024-09-16 18:00:15 -0500

    Update ResultsDetails.jsx
     - Fix cart item count on result / home page

--- client/src/components/main/home/results/details/ResultsDetails.jsx
+++ client/src/components/main/home/results/details/ResultsDetails.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import SearchContext from "../../../../../context/searchContext";
 import UserContext from "../../../../../context/userContext";
 import CartContext from "../../../../../context/cartContext";
@@ -6,7 +6,11 @@ import CartContext from "../../../../../context/cartContext";
 const ResultsDetails = () => {
     const { actions } = useContext(UserContext);
     const { results , resultsType } = useContext(SearchContext);
-    const { cart } = useContext(CartContext);
+    const { cart , cartActions } = useContext(CartContext);
+
+    useEffect(()=> {
+        cartActions.getCart();
+    }, [])
 
     return (
         <span className="w-100 pt-1 mt-2 mb-2  resultsDetails">
@@ -14,7 +18,7 @@ const ResultsDetails = () => {
                 <li className={`${resultsType === "featured" ? "d-flex":"d-none"} mt-1`}><h5 style={{fontFamily: "copal-std-outline, sans-serif"}}><b style={{fontFamily: "copal-std-solid, sans-serif", color: "#4d5c46"}}>Fea</b>tu<b style={{fontFamily: "copal-std-solid, sans-serif", color: "#bd7c71"}}>red</b></h5></li>
                 <li className={`${resultsType === "query" ? "d-flex":"d-none"} mt-1`}><h5 style={{fontFamily: "copal-std-outline, sans-serif"}}><b style={{fontFamily: "copal-std-solid, sans-serif", color: "#4d5c46"}}>Re</b>su<b style={{fontFamily: "copal-std-solid, sans-serif", color: "#bd7c71"}}>lts</b> {results.length}</h5></li>
                 <li className="ms-auto me-1"><button className="btn btn-dark py-1" style={{background: "#bd7c71", borderColor: "#bd7c71"}} onClick={ e => actions.navigate('/Home/Settings')}><i class="fa-solid fa-sliders"></i></button></li>
-                <li><button className="btn btn-dark py-1" onClick={ e => actions.navigate('/Home/Cart')}><b>{cart.length} Cart</b> <i class="fa-solid fa-cart-shopping"></i></button></li>
+                <li><button className="btn btn-dark py-1" onClick={ e => actions.navigate('/Home/Cart')}><b>{cart.length ?  cart.length:0} Cart</b> <i class="fa-solid fa-cart-shopping"></i></button></li>
             </ul>
         </span>
     )
